be16f67bd0155141a6f4498559fcaf3fb2566c6d
Ales Musil <amusil@redhat.com>
2023-03-30 13:32:46 +0200

    Flamegraph

--- ovn-fake-multinode-utils/scripts/perf.sh
+++ ovn-fake-multinode-utils/scripts/perf.sh
@@ -1,21 +1,26 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 host=$1
 node_name=$2
+
+function collect_perf_data() {
+    c=$1
+    mkdir ${host}-perf/$c
+    pid=$(docker exec $c /bin/sh -c "pidof -s perf")
+    docker exec $c /bin/sh -c "kill $pid && tail --pid=$pid -f /dev/null"
+    docker exec $c /bin/sh -c "perf script report flamegraph -o /tmp/ovn-perf.html"
+    docker cp $c:/tmp/ovn-perf.html ${host}-perf/$c/
+}
 
 mkdir /tmp/${host}-perf
 
 pushd /tmp
 for c in $(docker ps --format "{{.Names}}" --filter "name=${node_name}"); do
-    mkdir ${host}-perf/$c
-    docker exec $c /bin/sh -c "perf report -i /ovn-perf.data --stdio > /tmp/ovn-perf.log"
-    docker cp $c:/tmp/ovn-perf.log ${host}-perf/$c/
+    collect_perf_data $c
 done
 
 for c in $(docker ps --format "{{.Names}}" --filter "name=ovn-central"); do
-    mkdir ${host}-perf/$c
-    docker exec $c /bin/sh -c "perf report -i /ovn-perf.data --stdio > /tmp/ovn-perf.log"
-    docker cp $c:/tmp/ovn-perf.log ${host}-perf/$c/
+    collect_perf_data $c
 done
 
 tar cvfz ${host}-perf.tgz ${host}-perf
